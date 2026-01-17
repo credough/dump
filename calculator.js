d8e24df5729d44ea6bd3dd886d240eb8438fb5e0
Aaron Creed P. Celindro <aaroncreedpcelindro@gmail.com>
2026-01-17 19:21:07 +0800

    js practice

---
+++ calculator.js
@@ -0,0 +1,12 @@
+let a = 10;
+let b = 4;
+
+let addition = a + b;
+let subtraction = a - b;
+let multiplication = a * b;
+let division = a / b;
+
+console.log(addition);
+console.log(subtraction);
+console.log(multiplication);
+console.log(division);
