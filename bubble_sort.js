5ef27bd1492082ea30300d2e82bc0582bd9d4ae7
Aaron Creed P. Celindro <aaroncreedpcelindro@gmail.com>
2026-02-13 22:14:48 +0800

    bubble sort

---
+++ bubble_sort.js
@@ -0,0 +1,17 @@
+let arr = [5, 3, 8, 4, 2];
+
+for (let i = 0; i < arr.length; i++) {
+
+    for (let j = 0; j < arr.length - 1; j++) {
+
+        if (arr[j] > arr[j + 1]) {
+            let temp = arr[j];
+            arr[j] = arr[j + 1];
+            arr[j + 1] = temp;
+        }
+
+    }
+
+}
+
+console.log(arr);
