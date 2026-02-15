d5e213bb40adb28faea4839139645448c77a60e2
Aaron Creed P. Celindro <aaroncreedpcelindro@gmail.com>
2026-02-15 23:44:37 +0800

    find the largest num in an array

---
+++ find_largest_num.js
@@ -0,0 +1,11 @@
+let arr = [3, 9, 2, 15, 6];
+
+let largest = arr[0];
+
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > largest) {
+        largest = arr[i];
+    }
+}
+
+console.log("Largest:\n", largest)
